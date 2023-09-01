5c011d84115ee2aac34fb2cf8361f2b307673f6e
Yadunund <yadunund@gmail.com>
2023-09-02 00:23:13 +0530

    Style and add camera_idx to arg
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,23 @@ use clap::Parser;
 
 use opencv::{highgui, prelude::*, videoio, Result};
 
-use sensor_msgs::msg::Image as Image;
+use sensor_msgs::msg::Image;
 
 use core::slice;
 use std::env;
-use std::{error, mem, time::{Duration, SystemTime, UNIX_EPOCH}, thread::{self, sleep}};
+use std::{
+    error, mem,
+    thread::{self, sleep},
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct CommandLineArgs {
+    /// The index of the camera to open.
+    #[arg(short, long, default_value_t = 0)]
+    camera_idx: i32,
+
     /// The topic to publish the frames.
     #[arg(short, long, default_value_t = String::from("bgr8"))]
     encoding: String,
@@ -30,7 +38,6 @@ struct CommandLineArgs {
     topic: String,
 }
 
-
 fn main() -> Result<(), Box<dyn error::Error>> {
     // Get the CLI args for t
     let args = CommandLineArgs::parse();
@@ -42,24 +49,23 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         rclrs::spin(_spin_node);
     });
 
-
     let publisher = node.create_publisher::<Image>(&args.topic, rclrs::QOS_PROFILE_SENSOR_DATA)?;
 
-	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is the default camera
-	let opened = videoio::VideoCapture::is_opened(&cam)?;
-	if !opened {
-		panic!("Unable to open default camera!");
-	}
+    let mut cam = videoio::VideoCapture::new(args.camera_idx, videoio::CAP_ANY)?; // 0 is the default camera
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
+    if !opened {
+        panic!("Unable to open default camera!");
+    }
 
     let mut last_now = SystemTime::now();
-    let period = Duration::from_millis((1000.0 / args.rate) as u64 );
+    let period = Duration::from_millis((1000.0 / args.rate) as u64);
 
     while context.ok() {
-		let mut frame = Mat::default();
-		cam.read(&mut frame)?;
-		if frame.size()?.width == 0 {
+        let mut frame = Mat::default();
+        cam.read(&mut frame)?;
+        if frame.size()?.width == 0 {
             continue;
-		}
+        }
 
         let mut message = Image::default();
         let time_now = SystemTime::now();
@@ -69,7 +75,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
         message.height = frame.rows() as u32;
         message.width = frame.cols() as u32;
-        message.step = (frame.elem_size()? * ( message.width as usize )) as u32; // message.width;
+        message.step = (frame.elem_size()? * (message.width as usize)) as u32; // message.width;
 
         message.encoding = args.encoding.clone();
         message.is_bigendian = 1;
@@ -84,10 +90,12 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         publisher.publish(message)?;
 
         let sleep_till = last_now + period;
-        let sleep_for : Duration = sleep_till.duration_since(time_now).unwrap_or(Duration::from_secs(0));
+        let sleep_for: Duration = sleep_till
+            .duration_since(time_now)
+            .unwrap_or(Duration::from_secs(0));
         last_now = sleep_till;
         sleep(sleep_for);
     }
 
-	Ok(())
+    Ok(())
 }
